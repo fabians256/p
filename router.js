1dd9a46b5b71afcf67c7c0421273335c19a04537
fabians256 <168738759+fabians256@users.noreply.github.com>
2026-03-04 22:34:13 +0700

    Delete router.js

--- router.js
+++
@@ -1,9 +0,0 @@
-import { renderHome, renderSaved } from "./ui.js";
-import { state } from "./state.js";
-
-export function navigate(view) {
-  state.currentView = view;
-
-  if (view === "home") renderHome();
-  if (view === "saved") renderSaved();
-}
